fce455fff222a574c36da898aeb1c1b54ba5a0b6
saltorico <55649539+saltorico@users.noreply.github.com>
2025-10-15 23:56:22 +0200

    Remove duplicate RainLookahead definition

--- RainSentinel/RainSentinel/WeatherService.swift
+++ RainSentinel/RainSentinel/WeatherService.swift
@@ -1,32 +1,6 @@
 import Foundation
 import CoreLocation
 
-enum RainLookahead: Int, CaseIterable, Identifiable {
-    case sixHours = 6
-    case twelveHours = 12
-    case twentyFourHours = 24
-    case fortyEightHours = 48
-
-    var id: Int { rawValue }
-
-    var description: String {
-        switch self {
-        case .sixHours:
-            return "6 hours"
-        case .twelveHours:
-            return "12 hours"
-        case .twentyFourHours:
-            return "24 hours"
-        case .fortyEightHours:
-            return "48 hours"
-        }
-    }
-
-    var forecastDays: Int {
-        max(1, Int(ceil(Double(rawValue) / 24.0)))
-    }
-}
-
 struct RainForecast {
     struct DataPoint: Identifiable {
         let id = UUID()
